1bda95b3ac2192a0a5940a5bcefbec97e4fb0398
Felix Erdmann <hello@felixerdmann.com>
2022-05-10 19:02:55 +0200

    fix weird error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import type AppProps from 'next/app'
+import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 
 import '@fontsource/inter'
