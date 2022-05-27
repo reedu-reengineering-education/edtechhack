a6659c54908e59136782a58a4a56ce9c00bf2dea
Felix Erdmann <hello@felixerdmann.com>
2022-05-27 10:43:27 +0200

    update mentors and supporters

--- components/Supporters.tsx
+++ components/Supporters.tsx
@@ -5,22 +5,22 @@ const Supporters = () => {
       <div className="text-center">
         <h1 className="mt-12 text-3xl">Supporters</h1>
       </div>
-      {/* <div className="my-32 md:flex">
-        <div className="mx-12 flex-1 md:mx-36">
+      <div className="my-32 md:flex md:flex-wrap">
+        <div className="mx-12 my-8 flex-1 md:mx-36">
           <img
             src={require('@/assets/images/supporters/diwh.png').default.src}
             alt="cert"
             className="mx-auto max-w-xs"
           />
         </div>
-        <div className="mx-12 flex-1 md:mx-24 lg:mx-36">
+        <div className="mx-12 my-8 flex-1 md:mx-36">
           <img
             src={require('@/assets/images/supporters/bwk.png').default.src}
             alt="cert"
             className="mx-auto max-w-xs"
           />
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
