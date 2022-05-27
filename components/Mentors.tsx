fb6549c7942887d6818c176711fde6c0ef5d12a1
Felix Erdmann <hello@felixerdmann.com>
2022-05-27 09:37:41 +0200

    translate mentors

--- components/Mentors.tsx
+++ components/Mentors.tsx
@@ -1,74 +1,44 @@
 import Image from 'next/image'
-import Bar from './Elements/Bar'
+import { useRouter } from 'next/router'
 
 const Mentors = () => {
+  const router = useRouter()
+
+  const { mentors: t } = require(`@/assets/i18n/${router.locale}.json`)
+
+  const mentors = require(`@/assets/collections/mentors/${router.locale}.json`)
+
   return (
     <div className="my-32 lg:flex">
       <div className="visible mx-12 flex-1 lg:hidden">
         <h2 className="my-4 text-center text-2xl font-semibold lg:w-fit">
-          The Mentors and Mediators
+          {t.title}
         </h2>
-        <p className="text-center lg:w-fit">
-          General description about their role during the hackthon
-        </p>
+        <p className="text-center lg:w-fit">{t.description}</p>
       </div>
       <div className="flex flex-1 flex-wrap justify-center">
-        <div className="flex flex-col items-center p-4">
-          <div className="h-40 w-40 overflow-hidden rounded-full border-4 border-royal-blue">
-            <Image
-              src={require('@/assets/images/jury/barto_new.jpeg')}
-              layout="responsive"
-              alt="barto"
-            />
+        {mentors.map((m: any, i: number) => (
+          <div key={i} className="flex flex-col items-center p-4">
+            <div className="h-40 w-40 overflow-hidden rounded-full border-4 border-royal-blue bg-white">
+              <Image
+                src={require(`@/assets/images/mentors/${m.image}`)}
+                layout="responsive"
+                alt="mentor"
+              />
+            </div>
+            <h2 className="my-4 text-center text-xl font-semibold">{m.name}</h2>
+            <div className="text-center">
+              <p className="mb-4 w-40">{m.title}</p>
+              <p className="w-40">{m.tags}</p>
+            </div>
           </div>
-          <h2 className="my-4 text-center text-xl font-semibold">
-            Dr. Thomas Bartoschek
-          </h2>
-          <div className="text-center">
-            <p className="mb-4">Ph.D. Geoinformatik</p>
-            <p className="w-40">#Science #Education #Speaking #UX #Vision</p>
-          </div>
-        </div>
-        <div className="flex flex-col items-center p-4">
-          <div className="h-40 w-40 overflow-hidden rounded-full border-4 border-royal-blue">
-            <Image
-              src={require('@/assets/images/jury/barto_new.jpeg')}
-              layout="responsive"
-              alt="barto"
-            />
-          </div>
-          <h2 className="my-4 text-center text-xl font-semibold">
-            Dr. Thomas Bartoschek
-          </h2>
-          <div className="text-center">
-            <p className="mb-4">Ph.D. Geoinformatik</p>
-            <p className="w-40">#Science #Education #Speaking #UX #Vision</p>
-          </div>
-        </div>
-        <div className="flex flex-col items-center p-4">
-          <div className="h-40 w-40 overflow-hidden rounded-full border-4 border-royal-blue">
-            <Image
-              src={require('@/assets/images/jury/barto_new.jpeg')}
-              layout="responsive"
-              alt="barto"
-            />
-          </div>
-          <h2 className="my-4 text-center text-xl font-semibold">
-            Dr. Thomas Bartoschek
-          </h2>
-          <div className="text-center">
-            <p className="mb-4">Ph.D. Geoinformatik</p>
-            <p className="w-40">#Science #Education #Speaking #UX #Vision</p>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="mx-12 hidden flex-1 lg:block">
         <h2 className="my-4 text-center text-2xl font-semibold lg:w-fit">
-          The Mentors and Mediators
+          {t.title}
         </h2>
-        <p className="text-center lg:w-fit">
-          General description about their role during the hackthon
-        </p>
+        <p className="text-center lg:w-fit">{t.description}</p>
       </div>
     </div>
   )
