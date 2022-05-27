76fe2091ff605cf91a3b7d31e283889e72132df2
Felix Erdmann <hello@felixerdmann.com>
2022-05-27 09:20:58 +0200

    add date and place to main image

--- components/MainImage.tsx
+++ components/MainImage.tsx
@@ -7,45 +7,25 @@ const MainImage = () => {
   const { mainImage: t } = require(`@/assets/i18n/${router.locale}.json`)
 
   return (
-    <>
-      <div className="relative h-auto max-h-[40rem] w-full">
-        <div className=" max-h-[40rem] overflow-hidden">
-          <Image
-            src={require('@/assets/images/bg.png')}
-            alt="Logo"
-            layout="responsive"
-            className="overflow-hidden"
-          />
-        </div>
-        <div className="absolute left-0 bottom-0 w-full translate-y-2/3 md:translate-y-1/2">
-          <div className="container mx-auto p-4">
-            <div className="md:flex">
-              <div className="m-4 rounded-xl border-2 border-white bg-royal-blue p-4 text-white shadow-lg lg:mx-8 lg:p-12">
-                <p className="md:text-xl lg:text-2xl">{t.q1}</p>
-              </div>
-              <div className="m-4 rounded-xl border-2 border-white bg-royal-blue p-4 text-white shadow-lg lg:mx-8 lg:p-12">
-                <p className="md:text-xl lg:text-2xl">{t.q2}</p>
-              </div>
-            </div>
-          </div>
+    <div className="relative h-auto max-h-[40rem] w-full">
+      <div className="max-h-[40rem] overflow-hidden">
+        <Image
+          src={require('@/assets/images/bg.png')}
+          alt="Logo"
+          layout="responsive"
+          className="overflow-hidden"
+        />
+      </div>
+      <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center">
+        <div className="m-4 rounded-xl p-4 text-white shadow-lg backdrop-blur lg:mx-8 lg:p-12">
+          <p className="text-center text-2xl font-bold md:text-6xl lg:text-8xl">
+            {t.date}
+            <br />
+            {t.place}
+          </p>
         </div>
       </div>
-
-      {/* <div className="container mx-auto p-4 md:hidden">
-        <div className="m-4 rounded-xl border-2 border-white bg-royal-blue p-4 text-white shadow-lg lg:mx-8 lg:p-12">
-          <p className="text-xl">
-            Do you want to be part of tech-solutions for and national challenges
-            in the educational area ?
-          </p>
-        </div>
-        <div className="m-4 rounded-xl border-2 border-white bg-royal-blue p-4 text-white shadow-lg lg:mx-8 lg:p-12">
-          <p className="text-xl">
-            Do you have a problem in education that could be solved with the
-            support of technology?
-          </p>
-        </div>
-      </div> */}
-    </>
+    </div>
   )
 }
 
--- components/Program.tsx
+++ components/Program.tsx
@@ -7,52 +7,68 @@ const Program = () => {
   const { program: t } = require(`@/assets/i18n/${router.locale}.json`)
 
   return (
-    <div id="program" className="pt-14">
-      <div className="my-32 md:flex">
-        <div className="mx-12 flex-1">
-          <h2 className="my-4 text-center text-2xl font-semibold md:ml-auto md:w-fit">
-            {t.whatIs}
-          </h2>
-          <Button className="mx-auto md:ml-auto md:mr-0">
-            {t.button.join}
-          </Button>
-        </div>
-        <div className="mx-12 flex-1">
-          <h2 className="my-4 text-2xl font-semibold">{t.program.title}</h2>
-          <p>{t.program.description}</p>
+    <>
+      <div className="w-full md:-translate-y-1/2">
+        <div className="container mx-auto p-4">
+          <div className="md:flex">
+            <div className="m-4 rounded-xl border-2 border-white bg-royal-blue p-4 text-white shadow-lg lg:mx-8 lg:p-12">
+              <p className="md:text-xl lg:text-2xl">{t.q1}</p>
+            </div>
+            <div className="m-4 rounded-xl border-2 border-white bg-royal-blue p-4 text-white shadow-lg lg:mx-8 lg:p-12">
+              <p className="md:text-xl lg:text-2xl">{t.q2}</p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="my-32 md:flex">
-        <div className="mx-12 flex-1">
-          <h2 className="my-4 text-center text-2xl font-semibold md:ml-auto md:w-fit">
-            {t.whatKind}
-          </h2>
-          <Button className="mx-auto md:ml-auto md:mr-0">
-            {t.button.submit}
-          </Button>
+      <div id="program">
+        <div className="my-32 md:flex">
+          <div className="mx-12 flex-1">
+            <h2 className="my-4 text-center text-2xl font-semibold md:ml-auto md:w-fit">
+              {t.whatIs}
+            </h2>
+            <Button className="mx-auto md:ml-auto md:mr-0">
+              {t.button.join}
+            </Button>
+          </div>
+          <div className="mx-12 flex-1">
+            <h2 className="my-4 text-2xl font-semibold">{t.program.title}</h2>
+            <p>{t.program.description}</p>
+          </div>
         </div>
-        <div className="mx-12 flex-1">
-          <h2 className="my-4 text-2xl font-semibold">{t.challenges.title}</h2>
-          <p>{t.challenges.description}</p>
+        <div className="my-32 md:flex">
+          <div className="mx-12 flex-1">
+            <h2 className="my-4 text-center text-2xl font-semibold md:ml-auto md:w-fit">
+              {t.whatKind}
+            </h2>
+            <Button className="mx-auto md:ml-auto md:mr-0">
+              {t.button.submit}
+            </Button>
+          </div>
+          <div className="mx-12 flex-1">
+            <h2 className="my-4 text-2xl font-semibold">
+              {t.challenges.title}
+            </h2>
+            <p>{t.challenges.description}</p>
+          </div>
+        </div>
+        <div className="my-32 md:flex">
+          <div className="mx-12 flex-1">
+            <h2 className="my-4 text-center text-2xl font-semibold md:ml-auto md:w-fit">
+              {t.participate}
+            </h2>
+            <Button className="mx-auto md:ml-auto md:mr-0">
+              {t.button.apply}
+            </Button>
+          </div>
+          <div className="mx-12 flex-1">
+            <h2 className="my-4 text-2xl font-semibold">
+              {t.participants.title}
+            </h2>
+            <p>{t.participants.description}</p>
+          </div>
         </div>
       </div>
-      <div className="my-32 md:flex">
-        <div className="mx-12 flex-1">
-          <h2 className="my-4 text-center text-2xl font-semibold md:ml-auto md:w-fit">
-            {t.participate}
-          </h2>
-          <Button className="mx-auto md:ml-auto md:mr-0">
-            {t.button.apply}
-          </Button>
-        </div>
-        <div className="mx-12 flex-1">
-          <h2 className="my-4 text-2xl font-semibold">
-            {t.participants.title}
-          </h2>
-          <p>{t.participants.description}</p>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
