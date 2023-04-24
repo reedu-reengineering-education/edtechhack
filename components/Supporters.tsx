b51ee0c0f3639d0bbeb5ff4c3a1489d514c0ae83
Mario Pesch <mario.pesch@uni-muenster.de>
2023-04-24 17:35:48 +0200

    add supporter

--- components/Supporters.tsx
+++ components/Supporters.tsx
@@ -12,16 +12,22 @@ const Supporters = () => {
       <div className="text-center">
         <h1 className="mt-12 text-3xl">{t.title}</h1>
       </div>
-      <div className="md:flex md:flex-wrap">
-        <div className="mx-12 my-8 flex-1 md:mx-36">
-          <div className="mx-auto max-w-xs">
-            <Image
-              src={require('@/assets/images/supporters/dwih_english.png')}
-              alt="cert"
-              layout="responsive"
-              placeholder="blur"
-            />
-          </div>
+      <div className="items-center md:flex md:flex-wrap">
+        <div className="mx-12 flex-1 p-8 md:p-4 lg:p-8 xl:p-16">
+          <Image
+            src={require('@/assets/images/supporters/dwih_english.png')}
+            alt="cert"
+            layout="responsive"
+            placeholder="blur"
+          />
+        </div>
+        <div className="mx-12 flex-1 p-8 md:p-4 lg:p-8 xl:p-16">
+          <Image
+            src={require('@/assets/images/supporters/siemens_stiftung.png')}
+            alt="cert"
+            layout="responsive"
+            placeholder="blur"
+          />
         </div>
       </div>
     </div>
